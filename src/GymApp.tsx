5555a22b568300c6fe9266bbe35c058c09d2ae92
Cyril Kamgais <102966559+Kamgais@users.noreply.github.com>
2025-03-13 17:30:16 +0100

    .

--- src/GymApp.tsx
+++ src/GymApp.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-//import "swiper/css";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
